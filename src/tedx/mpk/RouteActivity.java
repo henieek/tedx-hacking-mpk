ce900115ea6ebc20970b02b4383c9dcbf2d14464
Mateusz Herych <heniek@applicake.com>
2012-09-20 22:12:07 +0200

    getting place from intent

--- src/tedx/mpk/RouteActivity.java
+++ src/tedx/mpk/RouteActivity.java
@@ -1,12 +1,19 @@
 
 package tedx.mpk;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import com.google.android.maps.*;
 
 import java.util.List;
 
-public class RouteActivity extends MapActivity {
+public class RouteActivity extends MapActivity implements LocationListener {
+
+    public static final String EXTRA_PLACE = "extra_place";
 
     private MapView mapView;
     private MapController mapController;
@@ -15,31 +22,56 @@ public class RouteActivity extends MapActivity {
     private RouteProvider routeProvider = new RouteProvider();
     private OverlaysFromRouteGenerator overlaysFromRouteGenerator = new OverlaysFromRouteGenerator();
 
+    private LocationManager locationManager;
+
+    private Place destination;
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        String provider = locationManager.getBestProvider(new Criteria(), true);
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
         setContentView(R.layout.activity_map);
 
         mapView = (MapView) findViewById(R.id.mapview);
         mapController = mapView.getController();
         overlays = mapView.getOverlays();
-
-        Place wawelCastle = new Place();
-        wawelCastle.setName("Wawel");
-        wawelCastle.setImageUrl("http://c.wrzuta.pl/wi4867/2b7f86d4001f73af4fd1e625/wawel_i_klasztory_w_krakowie");
-
-        overlays.clear();
-        overlays.addAll(overlaysFromRouteGenerator.getOverlaysForRoute(
-                routeProvider.getRouteFromMyLocation(new GeoPoint(50065608, 19946880), wawelCastle)));
-
-        mapView.setClickable(true);
-
+        destination = (Place) getIntent().getSerializableExtra(EXTRA_PLACE);
     }
 
     @Override
     protected boolean isRouteDisplayed() {
         return true;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        GeoPoint users = new GeoPoint((int) (location.getLatitude()*1e6), (int) (location.getLongitude()*1e6));
+        overlays.addAll(overlaysFromRouteGenerator.getOverlaysForRoute(
+                routeProvider.getRouteFromMyLocation(users, destination),
+                getResources().getDrawable(R.drawable.point_green)));
+
+        mapView.setClickable(true);
+
+        mapController.animateTo(users);
+        mapController.setZoom(15);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
 
