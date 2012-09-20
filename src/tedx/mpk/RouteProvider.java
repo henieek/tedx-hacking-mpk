9dd75628175da0de2a97a9ebb3b722334ad741a8
Mateusz Herych <heniek@applicake.com>
2012-09-20 19:24:22 +0200

    route provider

---
+++ src/tedx/mpk/RouteProvider.java
@@ -0,0 +1,12 @@
+package tedx.mpk;
+
+import android.webkit.GeolocationPermissions;
+import com.google.android.maps.GeoPoint;
+
+public class RouteProvider {
+
+    public Route getRouteFromMyLocation(GeoPoint usersLocation, Place place) {
+        return null;
+    }
+
+}
