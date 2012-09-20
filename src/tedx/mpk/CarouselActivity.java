9189198bce09a5608224ae358765d600081f9447
Marta Ryłko <rylko.marta@gmail.com>
2012-09-20 19:16:11 +0200

    added Carousel activity

---
+++ src/tedx/mpk/CarouselActivity.java
@@ -0,0 +1,14 @@
+package tedx.mpk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CarouselActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.carousel);
+    }
+    
+}
