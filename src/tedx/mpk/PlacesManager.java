afc33796424c1ee8e86dd87486658a97dfa3bb95
Marta Ryłko <rylko.marta@gmail.com>
2012-09-20 23:04:19 +0200

    updated places

--- src/tedx/mpk/PlacesManager.java
+++ src/tedx/mpk/PlacesManager.java
@@ -6,29 +6,33 @@ import java.util.List;
 public class PlacesManager {
 
     private static List<Place> PLACES;
-
+    private static final String baseUrl = "http://student.agh.edu.pl/~marta/hackaton/";
     static {
         PLACES = new ArrayList<Place>();
 
         Place miejsce = new Place();
+        miejsce.setName("Koœció³ Mariacki");
+        miejsce.setImageUrl(baseUrl + "1.jpg");
+        miejsce.setLatitude(50.06179);
+        miejsce.setLongitude(19.9371);
+        PLACES.add(miejsce);
+        miejsce = new Place();
+        miejsce.setName("MOCAK");
+        miejsce.setImageUrl(baseUrl + "2.jpg");
+        miejsce.setLatitude(50.047408);
+        miejsce.setLongitude(19.960862);
+        PLACES.add(miejsce);
+        miejsce = new Place();
+        miejsce.setName("Manggha");
+        miejsce.setImageUrl(baseUrl + "3.jpg");
+        miejsce.setLatitude(50.05096);
+        miejsce.setLongitude(19.93164);
+        PLACES.add(miejsce);
+        miejsce = new Place();
         miejsce.setName("Wawel");
-        miejsce.setImageUrl("http://c.wrzuta.pl/wi4867/2b7f86d4001f73af4fd1e625/wawel_i_klasztory_w_krakowie");
-        PLACES.add(miejsce);
-        miejsce = new Place();
-        miejsce.setName("Sukiennice");
-        miejsce.setImageUrl("http://www.infotuba.pl/upload/article/3927.jpg");
-        PLACES.add(miejsce);
-        miejsce = new Place();
-        miejsce.setName("Plac Centralny");
-        miejsce.setImageUrl("http://1.bp.blogspot.com/-dAj23WW33p0/TmtVftV6uJI/AAAAAAAAAD4/N7aKBPU9c7I/s1600/Plac_centralny.jpg");
-        PLACES.add(miejsce);
-        miejsce = new Place();
-        miejsce.setName("Muzeum Naradowe");
-        miejsce.setImageUrl("http://studente.pl/_cache/_thumbs/CF/cf03b8cc6ce874fd5fcf67ababf73970.jpg");
-        PLACES.add(miejsce);
-        miejsce = new Place();
-        miejsce.setName("Galeria Krakowska");
-        miejsce.setImageUrl("http://plfoto.com/zdjecia_new/1065187.jpg");
+        miejsce.setImageUrl(baseUrl + "4.jpg");
+        miejsce.setLatitude(50.05424);
+        miejsce.setLongitude(19.93551);
         PLACES.add(miejsce);
     }
 
