3d7eb349b9cacba7165e45c1ffbf6677c8b6d542
Mateusz Herych <heniek@applicake.com>
2012-09-20 19:25:50 +0200

    permissions for gps

--- src/tedx/mpk/RouteProvider.java
+++ src/tedx/mpk/RouteProvider.java
@@ -1,9 +1,11 @@
 package tedx.mpk;
 
-import android.webkit.GeolocationPermissions;
 import com.google.android.maps.GeoPoint;
 
 public class RouteProvider {
+
+
+
 
     public Route getRouteFromMyLocation(GeoPoint usersLocation, Place place) {
         return null;
